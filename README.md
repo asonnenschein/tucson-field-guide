11b2e2e504ea352d6065f2f444fdb03201380005
asonnenschein <adrian.sonnenschein@gmail.com>
2014-04-07 21:45:50 -0700

    mostly progress on basemap

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-tucson-field-guide
-==================
+###Tucson Field Guide
+
+A hobby project by Adrian Sonnenschein to catalogue places of historical and/or
+cultural significance in Tucson, Arizona and to plot those places on a map.
--- helper-scripts/shapefile_fields.py
+++ helper-scripts/shapefile_fields.py
@@ -1,32 +1,39 @@
+__author__ = 'Adrian Sonnenschein, 2014'
+
 import ogr
 
-root_path = '/Users/adrian/Documents/git/tucson-field-guide'
-input_file = root_path + '/data/shp/group_block_pima.shp'
-
-
 class EditShapefileAttrs(object):
+
+	'''
+	This class is used for automated data processing of shapefiles, 
+	specifically for use in the tucson-field-guide.  It relies on OGR.
+	'''
 
 	def __init__(self, shapefile):
 		self.driver = ogr.GetDriverByName('ESRI Shapefile')
 		self.data_source = self.driver.Open(shapefile, 1)
 		self.layer = self.data_source.GetLayer()
 
-	def filter_by_attrs(self, field, attribute):
-		query = field + " = '" + attribute + "'"
+	# Enumerate one field of a shapefile based on another field.  Ideally, the
+	# 'query_field' is a shared attribute among features and the 'target_field'
+	# will be populated with integer values starting at zero for each group
+	def enumerate_by_attrs(self, query_field, target_field, attribute):
+		counter = 0
+		query = qurey_field + " = '" + attribute + "'"
 		self.layer.SetAttributeFilter(query)
-#		length = len([i for i in self.layer])
 		
 		for feature in self.layer:
-			print feature.GetField('group')
+			feature.SetField(target_field, counter)
+			self.layer.SetFeature(feature)
+			counter += 1
 
+# Define some variables and put the code to work!
+root_path = '/Users/adrian/Documents/git/tucson-field-guide'
+input_file = root_path + '/data/shp/group_block_pima.shp'
+groups = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-a = EditShapefileAttrs(input_file)
-a.filter_by_attrs('group', 'z')
+shapefile = EditShapefileAttrs(input_file)
 
-
-
-#for feature in layer:
-#	print feature.GetField('group')
-#	if feature.GetField('group') is None:
-#		feature.SetField('group', 'l')
-#		layer.SetFeature(feature)
+for group in groups:
+	shapefile.enumerate_by_attrs('group', 'color_id' group)
